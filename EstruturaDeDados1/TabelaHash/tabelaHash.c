824bf5d38290e46b13690e2ed7c4f705311bfad6
LevoratoJoao <j.vitor.jvlds@gmail.com>
2022-11-09 12:23:26 -0300

    tabela hash finalizada

--- EstruturaDeDados1/TabelaHash/tabelaHash.c
+++ EstruturaDeDados1/TabelaHash/tabelaHash.c
@@ -37,41 +37,53 @@ int hash(int k) {
     return k % M;
 }
 
-int inserirElemento(TabelaHash *table, int k, char *name) {
+void inserirElemento(TabelaHash *table, int k, char *name) {
+    int posicao = hash(k);
     if (estaCheia(table) == 1) {
         printf("Tabela cheia\n");
-        return 1;
+        return;
     }
-    if (strcmp(table->elemento[hash(k)].nome, "") == 0)
-    {
-        table->elemento[hash(k)].info = k;
-        strcpy(table->elemento[hash(k)].nome, name);
-        table->quantidade = table->quantidade + 1;
-    } else if (strcmp(table->elemento[hash(k) + 1].nome, "") == 0) {
-        table->elemento[hash(k) + 1].info = k;
-        strcpy(table->elemento[hash(k) + 1].nome, name);
-        table->quantidade = table->quantidade + 1;
-    } else {
-        table->elemento[hash(k) - 1].info = k;
-        strcpy(table->elemento[hash(k) - 1].nome, name);
-        table->quantidade = table->quantidade - 1;
+    while (table->elemento[posicao].info != -1) {
+        posicao++;
+        if (posicao >= M) posicao = 0;
     }
+    table->quantidade++;
+    table->elemento[posicao].info = k;
+    strcpy(table->elemento[posicao].nome, name);
 }
 
 void imprimirTabelaHash(TabelaHash *table) {
     printf("Tabela:\n");
     for (int i = 0; i < M; i++) {
         if (estaVazia(table) == 1) {
-            printf("| [0%-2d] | NULL |\n", i);
+            printf("| [0%-2d]\t | NULL |\n", i);
         } else {
             printf("| [0%-2d] | %-10s |\n", i, table->elemento[i].nome);
         }
     }
 }
 
+int pesquisarElemento(TabelaHash *table, int chave) {
+    int posicao = hash(chave);
+    while (table->elemento[posicao].info != chave) {
+        posicao++;
+        if (posicao >= M) posicao = 0;
+        if (posicao == hash(chave)) return (-1);
+    }
+    return posicao;
+}
+
 int removerTabelaHash(TabelaHash *table, int chave) {
+    int posicao = pesquisarElemento(table, chave);
+    if (posicao < 0) {
+        printf("Elemento %d nao esta na tabela\n", chave);
+        return 1;
+    }
     table->quantidade--;
-    return (strcpy(table->elemento[chave].nome, ""));
+    table->elemento[posicao].info = -1;
+    strcpy(table->elemento[posicao].nome, "");
+
+    return 0;
 }
 
 int main() {
@@ -80,7 +92,6 @@ int main() {
     if(estaVazia(&table) == 1) {
         printf("Esta vazia\n");
     }
-    imprimirTabelaHash(&table);
     inserirElemento(&table, 100, "Tamara");
     inserirElemento(&table, 100, "Tamara2");
     inserirElemento(&table, 40, "Muriel");
@@ -88,9 +99,20 @@ int main() {
     inserirElemento(&table, 6, "Mantovani");
     inserirElemento(&table, 6, "Mantovani2");
     inserirElemento(&table, 0, "Viviane");
-    inserirElemento(&table, 0, "Viviane2");
-    imprimirTabelaHash(&table);
-    removerTabelaHash(&table, 1);
+    inserirElemento(&table, 0, "Viviane3");
+    inserirElemento(&table, 17, "Viviane4");
+    inserirElemento(&table, 15, "Viviane5");
+    inserirElemento(&table, 25, "Viviane6");
+    inserirElemento(&table, 96, "Viviane7");
+    inserirElemento(&table, 2, "Viviane2");
+    inserirElemento(&table, 63, "Viviane8");
+
+    int posicao = pesquisarElemento(&table, 2);
+    if (posicao < 0) printf("Elemento nao esta presente\n");
+    else printf("\nElemento encontrado:\n| [0%-2d] | %-10s |\n", posicao, table.elemento[posicao].nome);
+
+    removerTabelaHash(&table, 2);
+
     imprimirTabelaHash(&table);
 
     return 0;
