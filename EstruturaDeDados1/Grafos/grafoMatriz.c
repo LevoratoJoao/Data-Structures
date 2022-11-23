ee5322ba3a2f190df4d37c30d2650ee564fc8560
LevoratoJoao <j.vitor.jvlds@gmail.com>
2022-11-23 12:45:58 -0300

    continuando grafos

--- EstruturaDeDados1/Grafos/grafoMatriz.c
+++ EstruturaDeDados1/Grafos/grafoMatriz.c
@@ -21,6 +21,47 @@ void inicializarGrafo(GrafoMatriz *grafo, int vertices, bool dirigido) {
     } 
 }
 
+void inserirGrafo(GrafoMatriz *grafo, int verticeUm, int verticeDois) {
+    grafo->arestas++;
+    grafo->matriz[verticeUm - 1][verticeDois - 1] = 1;
+    if (grafo->dirigido == false) {
+        grafo->matriz[verticeDois - 1][verticeUm - 1] = 1;
+    }
+}
+
+void imprimirGrafo(GrafoMatriz *grafo) {
+    printf("Grafo:\n");
+    for (int i = 0; i < grafo->vertices; i++) { 
+        for (int j = 0; j < grafo->vertices; j++) { 
+            if (grafo->matriz[i][j]) { 
+                if (grafo->dirigido == true)
+                    printf("< %d, %d >\n", i + 1, j + 1);
+                else 
+                    printf("( %d, %d )\n", i + 1, j + 1); 
+            } 
+        } 
+    }
+}
+/*
+int existeAresta(GrafoMatriz *grafo, int verticeUm, int verticeDois) {
+    if (grafo->matriz[verticeUm - 1][verticeDois - 1] > 0) return grafo->matriz[verticeUm - 1][verticeDois - 1];
+    else return 0;
+}
+*/
+bool existeAresta(GrafoMatriz *grafo, int verticeUm, int verticeDois) {
+    return (grafo->matriz[verticeUm - 1][verticeDois - 1] > 0);
+}
+
+int buscaEmProfundidade(GrafoMatriz *grafo, int vertices, bool *visitado) {
+    visitado[vertices - 1] = true;
+    printf("%d\n", vertices);
+    for (int i = 0; i < grafo->vertices; i++) {
+        if (visitado[i] == false && grafo->matriz[vertices - 1][i] == 1) {
+            buscaEmProfundidade(grafo, i + 1, visitado);
+        }
+    }
+}
+
 void destruirGrafo(GrafoMatriz *grafo) {
     for (int i = 0; i < grafo->vertices; i++) free(grafo->matriz[i]);
     free(grafo->matriz);
@@ -30,6 +71,20 @@ int main() {
     GrafoMatriz grafo;
 
     inicializarGrafo(&grafo, 5, true);    
+    inserirGrafo(&grafo, 1, 2);
+    inserirGrafo(&grafo, 1, 3);
+    inserirGrafo(&grafo, 2, 3);
+    inserirGrafo(&grafo, 2, 5);
+    inserirGrafo(&grafo, 3, 4);
+    inserirGrafo(&grafo, 4, 5);
+    imprimirGrafo(&grafo);
+
+    if (existeAresta(&grafo, 3, 4) > 0)  
+        printf("Aresta existe\n");
+    else printf("Aresta nao existe\n");
+
+    bool *visitado = (bool*) calloc(grafo.vertices, sizeof(bool));
+    buscaEmProfundidade(&grafo, 1, visitado);
     destruirGrafo(&grafo);
 
     return EXIT_SUCCESS;
