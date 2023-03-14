f73e0fb2a8c1f26137cd1ef75f6343d1c6d3ebbc
LevoratoJoao <j.vitor.jvlds@gmail.com>
2023-03-14 18:18:42 -0300

    Selection sort inverse

--- Python/Sort/sortAlgorithm.py
+++ Python/Sort/sortAlgorithm.py
@@ -1,22 +1,28 @@
 class SortAlgorithm:
     def __init__(self, array):
         self.data = array
-    def bubbleSort(self):
+        
+    def bubbleSort(self, op):
         len(self.data)
         trade = True
         while (trade is True):
             trade = False
             for i in range(0, len(self.data) - 1):
-                if self.data[i] > self.data[i+1]:
+                if self.data[i] > self.data[i+1] and op == 1:
+                    self.data[i], self.data[i+1] = self.data[i+1], self.data[i]
+                    trade = True
+                if self.data[i] > self.data[i+1] and op == 0:
                     self.data[i], self.data[i+1] = self.data[i+1], self.data[i]
                     trade = True
         return self.data
 
-    def selectionSort(self):
+    def selectionSort(self, op):
         for i in range(0, len(self.data) - 1):
             lower = i
             for j in range(i + 1, len(self.data)):
-                if self.data[j] < self.data[lower]:
+                if self.data[j] < self.data[lower] and op == 1:
+                    lower = j
+                if self.data[j] > self.data[lower] and OP == 0:
                     lower = j
             self.data[i], self.data[lower] = self.data[lower], self.data[i]
         return self.data
